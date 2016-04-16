ad55b83cca6f9d30a09bd639a1ddc859c71234ca
James Wolff <james.wolff0@gmail.com>
2016-04-16 13:08:28 -0500

    Fixed a method I renamed

--- roboragi/AnimeBot.py
+++ roboragi/AnimeBot.py
@@ -186,7 +186,7 @@ async def process_message(message):
 async def on_message(message):
 	print('Message recieved')
 	#Is the message valid (i.e. it's not made by Discordoragi and I haven't seen it already). If no, try to add it to the "already seen pile" and skip to the next message. If yes, keep going.
-	if not (Search.isValidmessage(message)):
+	if not (Search.isValidMessage(message)):
 		try:
 			if not (DatabaseHandler.messageExists(message.id)):
 				DatabaseHandler.addMessage(message.id, message.author.name, message.channel, False)
