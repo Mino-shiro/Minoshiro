b1c8e888b3cdaaa6710e905a42eaf9f86203da34
James Wolff <james.wolff0@gmail.com>
2016-08-14 21:40:33 +0000

    Updated setup2.sh to only use requirements.txt

--- setup2.sh
+++ setup2.sh
@@ -1,9 +1,8 @@
-pyenv install 3.5.1
+pyenv install 3.5.2
 
 sudo apt-get install libffi-dev libxml2-dev libxslt-dev -y
 
 pyvenv venv
 source venv/bin/activate
 
-pip install git+https://github.com/Rapptz/discord.py@async
 pip install -r requirements.txt
