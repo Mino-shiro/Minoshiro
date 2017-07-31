5110cfbfcf69443cf544127dfd61cc54ff4b58ee
dashwav <dashwav@gmail.com>
2017-07-31 13:35:25 -0700

    setting up anilist(almost done with apis)

--- web_api/ani_list.py
+++ web_api/ani_list.py
@@ -1,6 +1,7 @@
 from itertools import chain
-from typing import Optional
-
+from typing import List, Optional
+from urllib.parse import quote
+from difflib import SequenceMatcher
 from session_manager import HTTPStatusError, SessionManager
 
 __escape_table = {
@@ -75,3 +76,102 @@ class AniList:
         async with resp:
             js = await resp.json()
             return js.get('access_token')
+
+    async def get_entry_details(
+            self,
+            session_manager: SessionManager,
+            medium: str,
+            query: str,
+            thing_id: str=None) -> Optional[dict]:
+        """
+        Get the details of an thing by search query.
+        :param session_manager: session manager object
+        :param medium: medium to search for 'anime', 'manga', 'novel'
+        :param query: the search term.
+        :param thing_id: thing id.
+        :return: dict with thing info.
+        """
+        self.access_token = await self.get_token()
+        clean_query = escape(query)
+        params = {
+            'access_token': self.access_token
+        }
+        try:
+            if medium == 'novel':
+                url = f'{self.base_url}/manga/search/{quote(clean_query)}'
+            else:
+                url = f'{self.base_url}/{medium}/search/{quote(clean_query)}'
+
+            print(url)
+            async with await session_manager.get(url, params=params) as resp:
+                if resp.status != 200:
+                    token = await self.get_token()
+                    async with await session_manager.get(
+                            url,
+                            params={'access_token': token}) as resp:
+                        thing = await resp.json()
+                else:
+                    thing = await resp.json()
+        except Exception as e:
+            session_manager.logger.warn(str(e))
+            return
+        for entry in thing:
+            if medium == 'manga' and 'novel' in entry['type'].lower():
+                thing.remove(entry)
+            elif medium == 'novel' and 'novel' not in entry['type'].lower():
+                thing.remove(entry)
+        return self.__get_closest(query, thing)
+
+    def __get_closest(self, query: str, thing_list: List[dict]) -> dict:
+        """
+        Get the closest matching anime by search query.
+        :param query: the search term.
+        :param thing_list: a list of animes.
+        :return: Closest matching anime by search query if found
+                    else an empty dict.
+        """
+        max_ratio, match = 0, None
+        matcher = SequenceMatcher(b=query.lower().strip())
+        for thing in thing_list:
+            ratio = self.__match_max(thing, matcher)
+            if ratio > max_ratio and ratio >= 0.90:
+                max_ratio = ratio
+                match = thing
+        return match or {}
+
+    def __match_max(self, thing: dict, matcher: SequenceMatcher) -> float:
+        """
+        Get the max matched ratio for a given thing.
+        :param thing: the thing.
+        :param matcher: the `SequenceMatcher` with the search query as seq2.
+        :return: the max matched ratio.
+        """
+        thing_name_list = []
+        thing_name_list_no_syn = []
+        max_ratio = 0
+        if 'title_english' in thing:
+            thing_name_list.append(thing['title_english'].lower())
+            thing_name_list_no_syn.append(thing['title_english'].lower())
+
+        if 'title_romaji' in thing:
+            thing_name_list.append(thing['title_romaji'].lower())
+            thing_name_list_no_syn.append(thing['title_romaji'].lower())
+
+        if 'synonyms' in thing:
+            for synonym in thing['synonyms']:
+                    thing_name_list.append(synonym.lower())
+
+        for name in thing_name_list:
+            matcher.set_seq1(name.lower())
+            ratio = matcher.ratio()
+            if ('one shot' in thing['type'].lower()):
+                ratio = ratio - .05
+            if ratio > max_ratio:
+                max_ratio = ratio
+            if thing['synonyms']:
+                for synonym in thing['synonyms']:
+                    matcher.set_seq1(synonym.lower())
+                    ratio = matcher.ratio()
+                    if ratio > max_ratio:
+                        max_ratio = ratio
+        return max_ratio
