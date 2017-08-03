fa3e59c3a59d5f5c7de82d7293338b1a1849a67d
Umi <982341777@qq.com>
2017-08-03 19:23:33 -0400

    Use the anidb data dump to find the anidb url

--- roboragi/web_api/ani_db.py
+++ roboragi/web_api/ani_db.py
@@ -3,63 +3,31 @@ Search AniDB for anime.
 """
 from difflib import SequenceMatcher
 from typing import List, Optional
-from urllib.parse import quote
 
-from pyquery import PyQuery
-
-from roboragi.session_manager import SessionManager
+from xmltodict import parse
 
 
-async def get_anime_url(
-        session_manager: SessionManager, query: str) -> Optional[str]:
+def process_xml(xml_string: str) -> List[dict]:
     """
-    Get anime url by search query.
-    :param session_manager: the `SessionManager` instance.
-    :param query: a search query.
-    :return: the anime url if it's found.
+    Process the xml string from the anidb data dump.
+    :param xml_string: the xml string.
+    :return: A list of dict with keys "id" and "titles".
     """
-    params = {
-        'task': 'search',
-        'query': quote(query)
-    }
-    try:
-        async with await session_manager.get(
-                'http://anisearch.outrance.pl/', params=params) as resp:
-            html = await resp.read()
-        pq = PyQuery(html)
-    except Exception as e:
-        session_manager.logger.warn(str(e))
-        return
-
-    anime_list = [
-        {'titles': titles, 'url': f'http://anidb.net/a{anime.attrib["aid"]}'}
-        for titles, anime in
-        [(__get_title_info(anime), anime) for anime in pq('animetitles anime')]
-        if titles
-    ]
-    return __get_closest(query, anime_list).get('url')
-
-
-def __get_title_info(anime) -> List[dict]:
-    """
-    Generate anime title info by anime.
-    :param anime: the anime object from `PyQuery`
-    :return: A list of anime title info.
-    """
+    parsed = parse(xml_string)
+    lst = parsed['animetitles']['anime']
     return [
-        {'title': title.text(), 'lang': title.attr['lang']}
-        for title in PyQuery(anime).find('title').items()
+        anime for anime in
+        (__format_anime(entry) for entry in lst)
+        if anime
     ]
 
 
-def __get_closest(query: str, anime_list: List[dict]) -> dict:
+def get_anime_url(query: str, anime_list: List[dict]) -> Optional[str]:
     """
-    Get the closest matching anime by search query.
-    :param query: the search term.
-    :param anime_list: a list of animes.
-
-    :return:
-        Closest matching anime by search query if found else an empty dict.
+    Get an anime url from a list of animes.
+    :param query: the search query.
+    :param anime_list: the list of animes.
+    :return: the anime url if found, else None.
     """
     max_ratio, match = 0, None
     matcher = SequenceMatcher(b=query.lower())
@@ -68,7 +36,31 @@ def __get_closest(query: str, anime_list: List[dict]) -> dict:
         if ratio > max_ratio and ratio >= 0.85:
             max_ratio = ratio
             match = anime
-    return match or {}
+    if match:
+        return (f'https://anidb.net/perl-bin/'
+                f'animedb.pl?show=anime&aid={match["id"]}')
+
+
+def __format_anime(anime_dict: dict) -> Optional[dict]:
+    """
+    Format an anime entry from the parsed xml string to a dict.
+    :param anime_dict: the input anime dict.
+    :return: a dict {"id": the anime id, "titles": the list of titles}
+    """
+    id_ = anime_dict.get('@aid')
+    titles = anime_dict.get('title')
+    if not titles or not id_:
+        return
+    try:
+        title_text = [t.get('#text') for t in titles]
+    except AttributeError:
+        _title = titles.get('#text')
+        if not _title:
+            return
+        title_text = [_title]
+    if not title_text:
+        return
+    return {'id': id_, 'titles': title_text}
 
 
 def __match_max(anime: dict, matcher: SequenceMatcher) -> float:
@@ -83,7 +75,7 @@ def __match_max(anime: dict, matcher: SequenceMatcher) -> float:
     """
     max_ratio = 0
     for title in anime['titles']:
-        matcher.set_seq1(title['title'].lower())
+        matcher.set_seq1(title.lower())
         ratio = matcher.ratio()
         if ratio > max_ratio:
             max_ratio = ratio
