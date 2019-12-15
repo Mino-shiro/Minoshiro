e993375a1f5b76ec414995b87c4bcfce9716fccd
Timothy Cyrus <4201229+tcyrus@users.noreply.github.com>
2019-12-15 15:55:19 -0500

    Update mu.py

--- minoshiro/web_api/mu.py
+++ minoshiro/web_api/mu.py
@@ -57,7 +57,7 @@ def get_manga_url_by_id(manga_id) -> str:
 
     :return: the manga url.
     """
-    return 'https://www.mangaupdates.com/series.html?id=' + str(manga_id)
+    return f'https://www.mangaupdates.com/series.html?id={manga_id!s}'
 
 
 def __get_closest(query: str, manga_list: List[dict], names) -> dict:
